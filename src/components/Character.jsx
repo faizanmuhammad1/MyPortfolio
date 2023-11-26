1548def04b6ac050cda4c9e38a5072f917614e17
Faizan Muhammad <faizanmuhammad6412@gmail.com>
2023-11-26 22:56:08 +0500

    Hosting My Webpage

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -4,21 +4,19 @@ import myImage from "../assets/eye.png";
 export default function Character() {
   return (
     <>
-      <div className="flex justify-center w-full">
+      <div className="relative flex justify-center w-full ">
         <img
           className="h-80 w-80"
           src="https://cn.i.cdn.ti-platform.com/content/20/the-amazing-world-of-gumball/showpage/za/gumball-carousel.a94b8e60.png"
           alt="MyProfile"
         />
-      </div>
-      <div>
         <img
-          className="absolute h-20 w-20 top-[40%] left-[52%] eye"
+          className="absolute h-20 w-20 top-[28%] left-[43%] eye"
           src={myImage}
           alt="image"
         />
         <img
-          className="absolute h-20 w-20 top-[37%] right-[50%] eye"
+          className="absolute h-20 w-20 top-[35%] right-[41%] eye"
           src={myImage}
           alt="image"
         />
