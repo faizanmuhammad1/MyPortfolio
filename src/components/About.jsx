82475f7984d104be9ed7f94b6b181006d2744153
Faizan Muhammad <faizanmuhammad6412@gmail.com>
2023-11-27 00:28:50 +0500

    madeitresponsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,13 +12,13 @@ const About = () => {
       <div className="text-center p-4">
         <h2 className="text-2xl font-bold mb-2 ">I am Faizan Muhammad 👨‍💻</h2>
       </div>
-      <div className="text-center text-1xl">
+      <div className="text-center text-sm	">
         <p>
           A passionate learner currently focusing on MERN Stack development.
           &#x2764;
         </p>
       </div>
-      <div className="flex justify-center gap-6 my-8">
+      <div className="flex justify-center gap-2 my-8">
         <SocialIcon url="https://github.com" />
         <SocialIcon url="https://x.com" />
         <SocialIcon url="https://leetcode.com" />
