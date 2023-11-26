5a30fe7ae0718a95c61ecc43376c45c742ef0497
Faizan Muhammad <faizanmuhammad6412@gmail.com>
2023-11-27 00:11:38 +0500

    fixedrelativeabsolute

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -4,19 +4,19 @@ import myImage from "../assets/eye.png";
 export default function Character() {
   return (
     <>
-      <div className="relative flex justify-center w-full ">
+      <div className="relative flex justify-center">
         <img
           className="h-80 w-80"
           src="https://cn.i.cdn.ti-platform.com/content/20/the-amazing-world-of-gumball/showpage/za/gumball-carousel.a94b8e60.png"
           alt="MyProfile"
         />
         <img
-          className="absolute h-20 w-20 top-[28%] left-[43%] eye"
+          className="absolute h-20 w-20 right-[17%] top-[36%] eye"
           src={myImage}
           alt="image"
         />
         <img
-          className="absolute h-20 w-20 top-[35%] right-[41%] eye"
+          className="absolute h-20 w-20 top-[28%] right-[49%] eye"
           src={myImage}
           alt="image"
         />
