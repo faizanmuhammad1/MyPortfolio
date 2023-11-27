adb52c0b3c0330c8bdc75751148e700f04e0bb3d
Faizan Muhammad <faizanmuhammad6412@gmail.com>
2023-11-28 00:07:15 +0500

    added animation to emojis

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,20 @@ import Character from "./Character";
 import { SocialIcon } from "react-social-icons";
 
 const About = () => {
+  const [isHovered, setIsHovered] = React.useState(false);
+  const [isHovered2, setIsHovered2] = React.useState(false);
+  const handleMouseOver = () => {
+    setIsHovered(true);
+  };
+  const handleMouseOver2 = () => {
+    setIsHovered2(true);
+  };
+  const handleMouseOut = () => {
+    setIsHovered(false);
+  };
+  const handleMouseOut2 = () => {
+    setIsHovered2(false);
+  };
   return (
     <section className="w-full">
       <div className="flex justify-center">
@@ -11,13 +25,18 @@ const About = () => {
       </div>
       <div className="text-center p-4">
         <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold mb-2 ">
-          I am Faizan Muhammad 👨‍💻
+          I am Faizan Muhammad{" "}
+          <span onMouseOver={handleMouseOver2} onMouseOut={handleMouseOut2}>
+            {isHovered2 ? "🙋" : "👨‍💻"}
+          </span>
         </h2>
       </div>
       <div className="text-center text-sm	md:text-xl lg:text-4xl">
         <p>
-          A passionate learner currently focusing on MERN Stack development.
-          &#x2764;
+          A passionate learner currently focusing on MERN Stack development.{" "}
+          <span onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            {isHovered ? "❤️" : "🖤"}
+          </span>
         </p>
       </div>
       <div className="flex justify-center gap-2 md:gap-4 lg:gap-6 my-8">
